29d47c5b0931418544d25cfff5ddbdc4025c4263
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-04-14 21:31:16 +0600

    added footer

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../components/shared/Navbar';
+import Footer from '../components/shared/Footer';
 
 const MainLayout = () => {
         return (
                 <div>
+                
                 <Navbar></Navbar>
+                <div className='min-h-[calc(100vh-70px)]'>
                 <Outlet></Outlet>
+                </div>
+                <div>
+                <Footer></Footer>
+                </div>
+                
                         
                 </div>
         );
