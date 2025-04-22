2004af60f7f1940ed62b7b36041c8e6fc3928bba
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-04-23 01:30:27 +0600

    axios interceptors integrated

--- src/components/hooks/useAxiosSecure.jsx
+++ src/components/hooks/useAxiosSecure.jsx
@@ -1,15 +1,62 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-        baseURL : import.meta.env.VITE_API_URL
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
 const useAxiosSecure = () => {
-        return (
-                <div>
-                        
-                </div>
-        );
+  const { logOut, setLoading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const requestInterceptor = axiosSecure.interceptors.request.use(
+      function (config) {
+        setLoading(true);
+        return config;
+      },
+      async (error) => {
+        setLoading(false);
+
+        return Promise.reject(error);
+      }
+    );
+
+    const responseInterceptor = axiosSecure.interceptors.response.use(
+      function (res) {
+        setLoading(false);
+        return res;
+      },
+      async function (error) {
+        const status = error.response?.status;
+
+        if (status === 401) {
+          try {
+            await logOut();
+            console.log("unauthorised access");
+          } catch (error) {
+            console.error("logout failed", error);
+          } finally {
+            navigate("/login");
+          }
+        } else if (status === 403) {
+          console.warn("forbidden access");
+        }
+        return Promise.reject(error);
+      }
+    );
+
+
+//     unmount==========
+    return () => {
+      axiosSecure.interceptors.request.eject(requestInterceptor);
+      axiosSecure.interceptors.response.eject(responseInterceptor);
+    };
+  }, [logOut, navigate, setLoading]);
+
+  return <div></div>;
 };
 
 export default useAxiosSecure;
